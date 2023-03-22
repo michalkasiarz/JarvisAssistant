3a1574c375de6bc66106024044de807f7969947c
Michal Kasiarz <michal.kasiarz@protonmail.com>
2023-03-22 11:24:30 +0100

    weather facade updated

--- weather_facade/weather_facade.py
+++ weather_facade/weather_facade.py
@@ -1,4 +1,4 @@
-from api.geocoding_api import GeocodingAPI
+from geopy.geocoders import Nominatim
 from api.weather_api import WeatherAPI
 from api.air_pollution_api import AirPollutionAPI
 
@@ -6,23 +6,21 @@ from api.air_pollution_api import AirPollutionAPI
 class WeatherFacade:
     def __init__(self, language, geocoding_api_key, weather_api_key, air_pollution_api_key):
         self.language = language
-        self.geocoding_api = GeocodingAPI(geocoding_api_key)
-        self.weather_api = WeatherAPI(weather_api_key)
-        self.air_pollution_api = AirPollutionAPI(air_pollution_api_key)
+        self.geocoding_api_key = geocoding_api_key
+        self.weather_api_key = weather_api_key
+        self.air_pollution_api_key = air_pollution_api_key
+        self.geolocator = Nominatim(user_agent = "weather_app")
+        self.weather_api = WeatherAPI(weather_api_key, language)
+        self.air_pollution_api = AirPollutionAPI(air_pollution_api_key, language)
 
     def get_weather_data(self, city):
-        lat, lon = self.geocoding_api.get_coordinates(city)
-        if lat is None or lon is None:
-            return None, None
+        location = self.geolocator.geocode(city)
+        if not location:
+            return None
 
-        weather_data = self.weather_api.get_weather(city, self.language)
+        lat, lon = location.latitude, location.longitude
+        weather_data = self.weather_api.get_weather_data(lat, lon)
         air_pollution_data = self.air_pollution_api.get_air_pollution_data(lat, lon)
 
         return weather_data, air_pollution_data
 
-    def get_coordinates(self, city):
-        results = self.geocoder.geocode(city)
-        if results:
-            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
-        else:
-            return None, None
